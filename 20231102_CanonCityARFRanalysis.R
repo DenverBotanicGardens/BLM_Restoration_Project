a3b73b1b935d9c71f8fbafdf3546eb8dee333a06
April Goebl <april.goebl@colorado.edu>
2023-12-03 21:53:22 -0700

    Update 20231102_CanonCityARFRanalysis.R

--- 20231102_CanonCityARFRanalysis.R
+++ 20231102_CanonCityARFRanalysis.R
@@ -88,6 +88,8 @@ boxplot(ARFR.Seedling.Count ~ Site + Contents, data=dats, col=cols, ylim=c(0,80)
 cols <- c(rep("darkseagreen4", 5), rep("red4",5), rep("steelblue4",6),
           rep("darkseagreen4", 5), rep("red4",5), rep("steelblue4",6))
 boxplot(ARFR.Seedling.Count ~ Contents + Site, data=dats, col=cols, ylim=c(0,75))
+boxplot(ARFR.Seedling.Count/NumSeeds ~ Contents + Site, data=dats, col=cols, ylim=c(0,0.14),
+        xlab=NA, ylab="Emergence Rate", xaxt="n", cex.lab=1.5)
 
 
 boxplot(ARFR.Seedling.Count ~ SeedMix + Site, data=dats)
