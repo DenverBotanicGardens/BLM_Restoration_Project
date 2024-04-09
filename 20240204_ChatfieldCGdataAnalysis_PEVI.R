9c7c22f998a1cefdb4b44e420c000460e97e27f9
April Goebl <april.goebl@botanicgardens.org>
2024-04-09 16:06:33 -0600

    Update 20240204_ChatfieldCGdataAnalysis_PEVI.R

--- 20240204_ChatfieldCGdataAnalysis_PEVI.R
+++ 20240204_ChatfieldCGdataAnalysis_PEVI.R
@@ -40,8 +40,10 @@ setwd("C:/Users/april.goebl/Denver Botanic Gardens/Conservation - Restoration/BL
 
 
 ## LOAD DATA --------------------------------------------------------------------------------------
-#PEVI.SdZn <- read.csv(file="AGoebl/Seeds/20231215_PEVI_LatLongSdZn_hexcodes.csv", sep=",", header=TRUE, dec=".")
+PEVI.LatLong <- read.csv(file="AGoebl/Seeds/20230127_PEVI_LatLong.csv", sep=",", header=TRUE, dec=".")
 PEVI23 <- read.csv(file="Chatfield/20240129_ChatfieldData2023_PEVI.csv", sep=",", header=TRUE, dec=".")
+
+GravelTemps <- read.csv(file="Chatfield/ClimateData/20240409_GravelTemps2022_ChatfieldCG.csv", sep=",", header=TRUE, dec=".")
 ## ----------------------------------------------------------------------------------------------
 
 
@@ -94,6 +96,17 @@ max(PEVI23$Flowering_20230808, na.rm=TRUE)
 
 
 ## PEVI - DATA MODS -----------------------------------------------------------------------------
+PEVI.LatLong$Source <- str_replace(PEVI.LatLong$SOURCE.CODE, "3-SOS", "")
+PEVI.LatLong$Source <- as.factor(PEVI.LatLong$Source)
+
+## Add Population name abbreviation column, W/in state ordered by decreasing latitude
+PEVI.LatLong$PopAbbrev[grepl("PEVI-WY932-29-11", PEVI.LatLong$Source)] = "P.WY.2"   
+PEVI.LatLong$PopAbbrev[grepl("PEVI-WY932-31-11", PEVI.LatLong$Source)] = "P.WY.1" 
+PEVI.LatLong$PopAbbrev[grepl("PEVI-CO932-227-10", PEVI.LatLong$Source)] = "P.CO.3"   
+PEVI.LatLong$PopAbbrev[grepl("PEVI-CO932-217-10", PEVI.LatLong$Source)] = "P.CO.4" 
+PEVI.LatLong$PopAbbrev[grepl("PEVI-CO932-214-10", PEVI.LatLong$Source)] = "P.CO.1"   
+PEVI.LatLong$PopAbbrev[grepl("PEVI-CO932-232-10", PEVI.LatLong$Source)] = "P.CO.2"   
+
 # ** obtain seed zones and biovars for PEVI **
 ## ----------------------------------------------------------------------------------------------
 
@@ -124,7 +137,6 @@ PEVI23$DaysToFlwr <- NA
 for (pp in 1:length(PEVI.PhenoCol.List)) {
   PEVI23$DaysToFlwr[PEVI23[,PEVI.PhenoCol.List[pp]]==1 & is.na(PEVI23$DaysToFlwr)] <- as.integer(PEVI.DaysToFlwr)[pp]
 }
-PEVI23 %>% group_by(Source) %>% dplyr::summarise(Pheno_Avg=mean(DaysToFlwr,na.rm=TRUE), Pheno_SD=sd(DaysToFlwr,na.rm=TRUE))
 
 ## Make a flowered Yes or No column
 PEVI23$FlwrYesNo <- NA 
@@ -133,5 +145,138 @@ PEVI23$FlwrYesNo[!is.na(PEVI23$DaysToFlwr)] <- 1                               #
 PEVI23$FlwrYesNo[is.na(PEVI23$DaysToFlwr) & (PEVI23$Survival_20230808==1 | PEVI23$Survival_20230718==1 |
                                              PEVI23$Survival_20230627==1)] <- 0  
 nrow(PEVI23[PEVI23$FlwrYesNo==0,])                                             #Num that survived but didn't flower 
-PEVI23 %>% group_by(Source) %>% dplyr::summarise(FlwrYesNo_Avg=mean(FlwrYesNo,na.rm=TRUE)) #Or could try sum and/or NUM=n()
 ## ---------------------------------------------------------------
+
+
+
+## CALCULATE RAW FLOWERING RESULTS BY SOURCE AND TREATMENT
+PEVI23 %>% group_by(Source) %>% dplyr::summarise(Pheno_Avg=mean(DaysToFlwr,na.rm=TRUE), Pheno_SD=sd(DaysToFlwr,na.rm=TRUE))
+PEVI23 %>% group_by(Source, Treatment) %>% dplyr::summarise(Pheno_Avg=mean(DaysToFlwr,na.rm=TRUE), Pheno_SD=sd(DaysToFlwr,na.rm=TRUE))
+# Most, but not all, pops in blk tx flowered earlier
+
+PEVI23 %>% group_by(Source) %>% dplyr::summarise(FlwrYesNo_Avg=mean(FlwrYesNo,na.rm=TRUE)) #Or could try sum and/or NUM=n()
+PEVI23 %>% group_by(Source, Treatment) %>% dplyr::summarise(FlwrYesNo_Avg=mean(FlwrYesNo,na.rm=TRUE))
+# All pops had higher prob of repro in blk tx
+
+
+
+
+## PEVI - TEST FOR SIGNIFICANCE OF TREATMENT EFFECT -------------------------------------
+## Flowering phenology
+hist(PEVI23$DaysToFlwr)
+PEVI.tx.DaystoFlwr <- glmer.nb(DaysToFlwr ~ Source + Treatment + (1|Block), data=PEVI23)
+PEVI.pop.DaystoFlwr <- glmer.nb(DaysToFlwr ~ Source + (1|Block), data=PEVI23)
+models <- list(PEVI.tx.DaystoFlwr, PEVI.pop.DaystoFlwr)
+mod.names <- c('IncldTx', 'JustPop')
+aictab(cand.set = models, modnames = mod.names )
+# Possible support for treatment 
+summary(PEVI.tx.DaystoFlwr)
+Anova(PEVI.tx.DaystoFlwr)
+
+## Probability of repro
+PEVI.tx.pRepro <- glmer(FlwrYesNo ~ Source + Treatment + (1|Block), data=PEVI23, family=binomial (link="logit"))
+PEVI.pop.pRepro <- glmer(FlwrYesNo ~ Source + (1|Block), data=PEVI23, family=binomial (link="logit"))
+models <- list(PEVI.tx.pRepro, PEVI.pop.pRepro)
+mod.names <- c('IncldTx', 'JustPop')
+aictab(cand.set = models, modnames = mod.names )
+# Possible support for treatment 
+summary(PEVI.tx.pRepro)
+Anova(PEVI.tx.pRepro)
+## ---------------------------------------------------------------
+
+
+
+
+
+## PEVI - VISUALIZE RAW DATA -----------------------------------------------------------------------------
+
+## Assign color based on latitude
+
+## 2023 Flowering data - Plots means as points and SE as error bars
+PEVI23.mn <- PEVI23 %>% group_by(Source) %>% dplyr::summarise(DaysToFlwr_MN=mean(DaysToFlwr,na.rm=TRUE),
+                                                              DaysToFlwr_SE=calcSE(DaysToFlwr), FlwrYesNo_MN=mean(FlwrYesNo,na.rm=TRUE),
+                                                              FlwrYesNo_SE=calcSE(FlwrYesNo))
+
+pop.list <- unique(PEVI23$Source)
+PEVI23.mn$PopCol <- NA
+PEVI23.mn$PopCol[grepl("PEVI-CO932-217-10", PEVI23.mn$Source)] = "#FEE08B"        
+PEVI23.mn$PopCol[grepl("PEVI-CO932-227-10", PEVI23.mn$Source)] = "#FFFF85"        
+PEVI23.mn$PopCol[grepl("PEVI-CO932-232-10", PEVI23.mn$Source)] = "#aabe7f"        
+PEVI23.mn$PopCol[grepl("PEVI-CO932-214-10", PEVI23.mn$Source)] = "#ABDDA4"     
+PEVI23.mn$PopCol[grepl("PEVI-WY932-29-11", PEVI23.mn$Source)] = "#66C2A5"    
+PEVI23.mn$PopCol[grepl("PEVI-WY932-31-11", PEVI23.mn$Source)] = "#3288BD"   
+
+#PEVI23.srcCol <- PEVI23 %>% select(Source, PopCol)
+PEVI23.mn <- left_join(PEVI23.mn, PEVI.LatLong, by="Source")
+
+PEVI23.mn <- PEVI23.mn[order(PEVI23.mn$Lat),]
+plot(c(1:6), PEVI23.mn$FlwrYesNo_MN, col="black", bg=PEVI23.mn$PopCol, pch=21, cex=2.5, ylab="Flowering rate",xlab="Population",
+     xaxt="n",cex.lab=1.5, ylim=c(0.45,0.9))
+arrows(c(1:6), PEVI23.mn$FlwrYesNo_MN+PEVI23.mn$FlwrYesNo_SE, c(1:6), PEVI23.mn$FlwrYesNo_MN-PEVI23.mn$FlwrYesNo_SE,
+       angle=90, length=0, col="grey")
+axis(1,at=1:6,labels=PEVI23.mn$PopAbbrev,las=1, cex.axis=1.1, col.ticks="black")
+
+
+plot(c(1:6), PEVI23.mn$DaysToFlwr_MN, col="black", bg=PEVI23.mn$PopCol, pch=21, cex=2.5, ylab="Days to first flower",xlab="Population",
+     xaxt="n",cex.lab=1.5, ylim=c(141,146)) #bg=PEVI23.mn$HexCode
+arrows(c(1:6), PEVI23.mn$DaysToFlwr_MN+PEVI23.mn$DaysToFlwr_SE, c(1:6), PEVI23.mn$DaysToFlwr_MN-PEVI23.mn$DaysToFlwr_SE,
+       angle=90, length=0, col="grey")
+axis(1,at=1:6,labels=PEVI23.mn$PopAbbrev,las=1, cex.axis=1.1, col.ticks="black")
+
+legend("center", legend=PEVI23.mn$PopAbbrev[order(PEVI23.mn$Lat, decreasing=TRUE)], col="black",
+       pt.bg=PEVI23.mn$PopCol[order(PEVI23.mn$Lat, decreasing=TRUE)], cex=1.9, pch=21)
+
+
+
+
+## 2023 Flowering data by source and tx - Plots means as points and SE as error bars
+PEVI23.mnTx <- PEVI23 %>% group_by(Source, Treatment) %>% dplyr::summarise(DaysToFlwr_MN=mean(DaysToFlwr,na.rm=TRUE),
+                                                              DaysToFlwr_SE=calcSE(DaysToFlwr), FlwrYesNo_MN=mean(FlwrYesNo,na.rm=TRUE),
+                                                              FlwrYesNo_SE=calcSE(FlwrYesNo))
+
+PEVI23.mnTx$PopCol <- NA
+PEVI23.mnTx$PopCol[grepl("PEVI-CO932-217-10", PEVI23.mnTx$Source)] = "#FEE08B"        
+PEVI23.mnTx$PopCol[grepl("PEVI-CO932-227-10", PEVI23.mnTx$Source)] = "#FFFF85"        
+PEVI23.mnTx$PopCol[grepl("PEVI-CO932-232-10", PEVI23.mnTx$Source)] = "#aabe7f"        
+PEVI23.mnTx$PopCol[grepl("PEVI-CO932-214-10", PEVI23.mnTx$Source)] = "#ABDDA4"     
+PEVI23.mnTx$PopCol[grepl("PEVI-WY932-29-11", PEVI23.mnTx$Source)] = "#66C2A5"    
+PEVI23.mnTx$PopCol[grepl("PEVI-WY932-31-11", PEVI23.mnTx$Source)] = "#3288BD"   
+
+PEVI23.mnTx <- left_join(PEVI23.mnTx, PEVI.LatLong, by="Source")
+
+
+PEVI23.mnTx <- PEVI23.mnTx[order(PEVI23.mnTx$Lat),]
+symbs<-rep(c(22,25),6)
+xSpac <- (1:12)-rep(c(0.5,1),6) #Change spacing to cluster population pairs
+
+plot(xSpac, PEVI23.mnTx$FlwrYesNo_MN, col="black", bg=PEVI23.mnTx$PopCol, pch=symbs, cex=2.5, ylab="Flowering rate",xlab="Population",
+     xaxt="n",cex.lab=1.5, ylim=c(0.35,0.95))
+arrows(xSpac, PEVI23.mnTx$FlwrYesNo_MN+PEVI23.mnTx$FlwrYesNo_SE, xSpac, PEVI23.mnTx$FlwrYesNo_MN-PEVI23.mnTx$FlwrYesNo_SE,
+       angle=90, length=0, col="grey")
+axis(1,at=xSpac,labels=PEVI23.mnTx$PopAbbrev,las=1, cex.axis=1.1, col.ticks="white")
+
+xSpac <- (1:12)-rep(c(0.5,1.2),6) #Change spacing to cluster population pairs
+plot(xSpac, PEVI23.mnTx$DaysToFlwr_MN, col="black", bg=PEVI23.mnTx$PopCol, pch=symbs, cex=2.5, ylab="Days to first flower",xlab="Population",
+     xaxt="n",cex.lab=1.5, ylim=c(140,148)) 
+arrows(xSpac, PEVI23.mnTx$DaysToFlwr_MN+PEVI23.mnTx$DaysToFlwr_SE, xSpac, PEVI23.mnTx$DaysToFlwr_MN-PEVI23.mnTx$DaysToFlwr_SE,
+       angle=90, length=0, col="grey")
+axis(1,at=xSpac,labels=PEVI23.mnTx$PopAbbrev,las=1, cex.axis=1.1, col.ticks="white")
+
+plot.new()
+legend("center", legend=c("Warm", "Cool"), col="black",
+       pt.bg="black", cex=1.9, pch=c(22,25))
+## -------------------------------------------------------------------
+
+
+
+
+## MAKE PLOT OF GRAVEL SURFACE TEMPS
+GravTmp.mn <- GravelTemps %>% group_by(Tx) %>% dplyr::summarise(TEMP_MN=mean(Reading_DegC,na.rm=TRUE), TEMP_SE=calcSE(Reading_DegC))
+
+#ERNA23.mn <- ERNA23.mn[order(ERNA23.mn$AliveYesNo_MN),]
+barXvals<-barplot(GravTmp.mn$TEMP_MN, xlab=NA, ylab="Temperature (°C)", cex.lab=1.4, las=1, 
+                  names=c("Black gravel","No gravel","White gravel"), main="Soil surface temperatures", cex.main=1.5, 
+                  col=c("black","grey","white"), cex.names=1.1, ylim=c(0,110))
+arrows(barXvals, GravTmp.mn$TEMP_MN+GravTmp.mn$TEMP_SE, barXvals, GravTmp.mn$TEMP_MN-GravTmp.mn$TEMP_SE,
+       angle=90, length=0, col="black")
+## -------------------------------------------------------------------
